c1004f8c4c5cc57167ba418fa6507c17f997ecc7
sriedernew <susann.rieder@gmx.de>
2018-06-12 23:29:50 +0200

    Racket movement

---
+++ Scripts/PlayerTwoRacket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoRacket : MonoBehaviour {
+    public float movementSpeed = 100;
+    public GameObject ball;
+
+    private void FixedUpdate()
+    {
+       //if (PlayerPrefs.GetString("idAI")==false) {
+            float axis = Input.GetAxisRaw("Vertical2");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, axis) * movementSpeed;
+       // }
+    }
+}
